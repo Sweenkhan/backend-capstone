9199b186e71a995eaad8f01c978dc844aef622fd
sween <khansween@gmail.com>
2023-07-21 23:56:26 +0530

    added book schema

--- models/book.js
+++ models/book.js
@@ -1,34 +1,34 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
 
-// const bookSchema = new mongoose.Schema({
+const bookSchema = new mongoose.Schema({
      
-//     title:{
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     author:{
-//         type: String, 
-//         required: true,
-//         unique: false
-//     },  
-//      image:{
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     bookType:{
-//         type: String,
-//         required: true,
-//     },
-//     country: {
-//         type: String,
-//         required: true
-//     },
-// })
+    title:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    author:{
+        type: String, 
+        required: true,
+        unique: false
+    },  
+     image:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    bookType:{
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true
+    },
+})
 
  
-// const book = mongoose.model("Book", bookSchema);
+const book = mongoose.model("Book", bookSchema);
 
-// export default book
+export default book
