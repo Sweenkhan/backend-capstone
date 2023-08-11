01fb88fbcc269192fac9e1907abbc6c4499be6dc
sween <khansween@gmail.com>
2023-08-11 22:29:47 +0530

    changes in schema

--- models/book.js
+++ models/book.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
  
-const bookSchema = new mongoose.Schema({
-     
+const bookSchema = new mongoose.Schema({ 
     title:{
         type: String,
         required: true,
