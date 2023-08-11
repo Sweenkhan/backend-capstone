b58b055c6384409df54571a51705699789c39d30
sween <khansween@gmail.com>
2023-08-11 22:27:06 +0530

    made schema for dashboard

---
+++ models/dashboard.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const dashboardSchema = new mongoose.Schema({
+    userId:{
+        type: String,
+        required: true,
+        unique: true 
+    },
+    likedBooks:{
+        type : Array, 
+        unique: false,
+    },
+    completedReadBooks:{
+        type: Array, 
+        unique: false,
+    },
+    comentedBooks:{
+       type: Array,
+       unique: false, 
+    },
+    currentRead:{
+        type: Array, 
+        unique: false,
+    }
+})
+
+const dashboard = mongoose.model("dashboard", dashboardSchema);
+
+export default dashboard
