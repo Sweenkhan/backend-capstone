2dbcb148332d0f2a12de3b845af4c23635afada7
sween <khansween@gmail.com>
2023-08-13 19:01:30 +0530

    added authentication middleware

---
+++ auth/authenticat.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+
+config()
+
+
+function authentication (req, res, next){
+
+    const session = req.body.session;
+
+  if (!session) {
+    res.status(401).send("failed auther");
+  }
+
+  try {
+    const decodeToken = jwt.verify(session, process.env.JWT_SECRET);
+    const userName = decodeToken.userName;
+
+    if (userName) {
+
+    //   const userDashboard = await dashboard.findOne({ userName})
+    //   console.log(userDashboard.userName);
+        
+          next() 
+    } else {
+      res.status(300).send("failed auther");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(300).send("failed auther");
+  }
+}
+
+export default authentication;
