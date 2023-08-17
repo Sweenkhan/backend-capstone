7f9cb55ecf6c6eb85e58f5762137c82879ae0ce1
sween <khansween@gmail.com>
2023-08-17 20:56:03 +0530

    added rating route

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,8 +1,9 @@
-import  express  from "express";
+import express from "express";
 import book from "../models/book.js";
+import authentication from "../auth/authenticat.js";
+import dashboard from "../models/dashboard.js";
 
-const router = express.Router()
-
+const router = express.Router();
 
 //----------------------GET ALL BOOKS-----------------//
 router.get("/book", async (req, res) => {
@@ -15,7 +16,6 @@ router.get("/book", async (req, res) => {
   }
 });
 
-
 //------------------SEARCH-BOOKS------------------//
 router.post("/search", async (req, res) => {
   try {
@@ -26,6 +26,66 @@ router.post("/search", async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: "An error occurred while searching." });
   }
-}); 
+});
 
-export default router
+//---------------RATING-BOOKS------------------//
+
+router.patch("/rating", authentication, async (req, res) => {
+  let bookid = req.body.ratingBook;
+  let username = req.authUsername;
+  let rated = req.body.rating;
+
+  let filter = await dashboard.findOne({ username });
+  let ratingBooks = filter.ratingBooks;
+
+  //CHECKING BOOK ALREADY HAS GIVEN RATING
+  function ratedBooks() {
+    let exist = false;
+    for (let i = 0; i < ratingBooks.length; i++) {
+      if (ratingBooks[i].bookId === bookid) {
+        exist = true;
+      } else {
+        exist = false;
+      }
+    }
+
+    return exist;
+  }
+
+  let ratingExist = ratedBooks();
+  console.log(bookid, rated, ratingExist);
+
+  if (!ratingExist) {
+    await dashboard.updateOne(
+      { username },
+      { $push: { ratingBooks: { bookId: bookid, rating: rated } } }
+    );
+    res.status(200).send("succesfuly rated books");
+  } else {
+    res.status(200).send("You already gave rating to this book");
+  }
+});
+
+//-----------------------------------COMPLETED BOOK-----------------------
+
+router.patch("/completed", authentication, async (req, res) => {
+  let bookid = req.body.completedBook;
+  let username = req.authUsername;
+
+  let filter = await dashboard.findOne({ username });
+  let completedReadBooks = filter.completedReadBooks;
+
+  if (completedReadBooks.includes(bookid)) {
+    console.log("kr diya tha")
+    res.status(200).send("You already gave rating to this book");
+  } else {
+    console.log("kr diya")
+    await dashboard.updateOne(
+      { username },
+      { $push: { completedReadBooks: bookid } }
+    );
+    res.status(200).send("succesfuly completed read books");
+  }
+});
+
+export default router;
