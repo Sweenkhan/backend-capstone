cb17479e4a447f3941b95bf5d202a952b801dd15
sween <khansween@gmail.com>
2023-08-19 04:18:06 +0530

    add all get routes in back-end

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -16,13 +16,13 @@ router.get("/dashboard", authentication, async (req, res) => {
   });
  
 
+ //-----------------------------ALL LIKED BOOKS------------------------// 
 router.get("/getAllLikedBooks", authentication, async(req, res) =>{
 
   let username = req.authUsername;
   const filterUser = await dashboard.findOne({username})
   let likedBooksIds = filterUser.likedBooks.slice(1)
   
-    
   const collectData = await Promise.all(likedBooksIds.map(async (_id) => {
     return await book.findOne({ _id });
   }));
@@ -32,6 +32,56 @@ router.get("/getAllLikedBooks", authentication, async(req, res) =>{
   res.status(200).send(collectData)
 
 }) 
+
+
+//------------------------------------GET ALL CURRENT READ BOOKS------------------------ 
+
+router.get("/getAllReadBooks", authentication, async(req, res) =>{
+     
+  let username = req.authUsername;
+  const filterUser = await dashboard.findOne({username})
+  let currentReadIds = filterUser.currentRead.slice(1);
+
+  const collectData = await Promise.all(currentReadIds.map(async(_id) =>{
+    return await book.findOne({ _id });
+  }))
+ 
+  res.status(200).send(collectData)
+});
+
+
+//-----------------------------GET ALL COMPLETED READ BOOKS---------------------------- 
+router.get("/getAllCompletedBooks", authentication, async(req, res) =>{
+
+  let username = req.authUsername;
+  const filterUser = await dashboard.findOne({username}); 
+  let completedBookIds = filterUser.completedReadBooks.slice(1)
+
+  const collectData = await Promise.all(completedBookIds.map(async(_id) =>{
+    return await book.findOne({ _id });
+  }))
+  res.status(200).send(collectData);
+})
+
+
+//---------------------------------GET ALL RATED BOOKS-------------------------------
+router.get('/getAllRatedBooks', authentication, async(req, res) =>{
+  let username = req.authUsername;
+  const filterUser = await dashboard.findOne({username}); 
+  let ratingBookIds = filterUser.ratingBooks.slice(1)
+
+ 
+  let temp =  ratingBookIds.map((books) =>{
+              return books.bookId
+    }) 
+   
+ 
+  const collectData = await Promise.all(temp.map(async(_id) =>{ 
+    return await book.findOne({_id});
+  }))
+   
+  res.status(200).send(collectData)
+})
 
 
 export default router;  
