67098f29ddd83d7acd5fc2aacb7a92ea71e55d89
sween <khansween@gmail.com>
2023-08-21 03:16:29 +0530

    new frinedlist schema add

---
+++ models/friendlist.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const friendListSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+    },
+    sendRequest:{
+        type: Array,
+        unique: false,
+    },
+    acceptRequest:{
+        type: Array,
+        unique: false,
+    },
+    friendList: {
+        type: Array,
+        unique: false,
+    }
+})
+
+const friendlist = mongoose.model("friendlist", friendListSchema);
+
+export default friendlist
