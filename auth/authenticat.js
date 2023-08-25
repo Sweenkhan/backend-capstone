085349e1da5b1e4a858b614d1c73a0e90950b4b9
sween <khansween@gmail.com>
2023-08-26 00:15:59 +0530

    changed for checking

--- auth/authenticat.js
+++ auth/authenticat.js
@@ -8,29 +8,29 @@ function authentication(req, res, next) {
   const session = req.body.session || req.header("authorization");
 
   console.log(session)
-  next();
-  // if (!session) { 
-  //   res.send({status:400, message: "Didn't get any SessionId"}) 
-  // }
+  // next();
+  if (!session) { 
+    res.send({status:400, message: "Didn't get any SessionId"}) 
+  }
 
-  // try {
-  //   console.log("reached here");
-  //   const decodeToken = jwt.verify(session, process.env.JWT_SECRET);
-  //   console.log(decodeToken)
-  //   const userName = decodeToken.userName;
+  try {
+    console.log("reached here");
+    const decodeToken = jwt.verify(session, process.env.JWT_SECRET);
+    console.log(decodeToken)
+    const userName = decodeToken.userName;
     
-  //   if (userName) {
-  //     req.authUsername = userName;
-  //     next();
-  //   } else {
-  //     // res.status(300).send("failed auther");
-  //     res.send({status:300, message: "failed authentication"}) 
-  //   }
-  // } catch (err) {
-  //   console.log(err);
-  //   // res.status(301).send("failed auther");
-  //   res.send({status:301, message: "Unknown User"}) 
-  // }
+    if (userName) {
+      req.authUsername = userName;
+      next();
+    } else {
+      // res.status(300).send("failed auther");
+      res.send({status:300, message: "failed authentication"}) 
+    }
+  } catch (err) {
+    console.log(err);
+    // res.status(301).send("failed auther");
+    res.send({status:301, message: "Unknown User"}) 
+  }
 }
 
 export default authentication;
