7dd2ccabdeeb931dd678c2a49387dd0d7ac842ee
sween <khansween@gmail.com>
2023-08-25 12:30:03 +0530

    added new front-end cors

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:3000" }));
+// app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://bookshelf-8yfg.onrender.com" }));
 app.use(userRouter);
 app.use(dashboardRouter);
 app.use(bookRouter);
